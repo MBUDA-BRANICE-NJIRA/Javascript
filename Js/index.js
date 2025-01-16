6b6e5f6053946e54b9780e867d36d1e9466c19a3
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-01-16 16:22:56 +0300

    Learnt On Variables

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,19 @@
-alert("Javascript");
+// alert("Javascript")
+
+//SYNTAX
+
+//This are simply the rules of Javascript
+//-Javascript is case sensitive
+//Javascript is always written in lowercase
+//Evey statement always ends wth a semi collon
+
+
+//Variables
+var myAge = 25;
+var myName = "John Doe";
+//Variables are used to store values in Javascript
+//Variables are declared with the var keyword
+//Variables can be reassigned with the var keyword
+//Variables can be declared with the let keyword
+//Variables can be declared with the const keyword
+//Variables can be declared with the var keyword and then reassigned with the let keyword
