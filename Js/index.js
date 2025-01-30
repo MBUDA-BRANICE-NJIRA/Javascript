40862573e8199c8b5e261815755585e2845a2eab
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-01-30 09:41:18 +0300

    Created  onclick javascript function using else if statement in the function elemeny

--- Js/index.js
+++ Js/index.js
@@ -264,7 +264,23 @@
 
 // onclick event
 
+var content = document.getElementById("content");
+var button =  document.getElementById("show-more");
 
+button.onClick = function() {
+    if (content.className == "open") {
+        //shrink the box
+        content.className = "";
+        button.innerHTML = "Show More";
+        
+    } else {
+        // expand the box
+        content.className == "open";
+        button.innerHTML = "Show Less";
+
+    }
+    
+}
 
 
 
